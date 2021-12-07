7954c58e00649300ab9321e47d45ae4d2e87d07a
j1arm1 <90942808+j1arm1@users.noreply.github.com>
2021-12-07 15:20:39 -0500

    Update 1.3.3_Gestion_Usuarios.js

--- src/components/1.3.3_Gestion_Usuarios.js
+++ src/components/1.3.3_Gestion_Usuarios.js
@@ -20,4 +20,61 @@ const GestionUsuarios = function (props) {
 
 }
 
+export default GestionUsuarios;import { Fragment } from "react";
+import React, {useState} from 'react';
+import dataUsuarios from './Data/dataUsuarios';
+
+
+const GestionUsuarios = function (props) {
+    const [data, setData] = useState(dataUsuarios);
+    const [modalEditar, setModalEditar] = useState(false);
+    const [modalEliminar, setModalEliminar] = useState(false);
+    const [modalInsertar, setModalInsertar] = useState(false);
+  
+    const [usuarioSeleccionado, setUsuarioSeleccionado] = useState({
+      id: '',
+      usuario: '',
+      nombres: '',
+      contrasena: '',
+      rol: ''
+    });
+    return (
+        
+        <div className="App">
+        <h2>Información de Usuarios</h2>
+        <br />
+      <button className="btn btn-success" >Insertar</button>  {"   "} 
+      <br /><br />
+        <table className="table table-bordered">
+          <thead>
+            <tr>
+              <th>ID</th>
+              <th>Usuario</th>
+              <th>Nombres</th>
+              <th>Rol</th>
+              <th>Acciones</th>
+            </tr>
+          </thead>
+          <tbody>
+          {data.map(elemento=>(
+            <tr>
+              <td>{elemento.id}</td>
+              <td>{elemento.usuario}</td>
+              <td>{elemento.nombres}</td>
+              <td>{elemento.rol}</td>
+              <td><button className="btn btn-primary" >Editar</button> {"   "} 
+              <button className="btn btn-danger" >Eliminar</button>{"   "} 
+              </td>
+            </tr>
+          ))
+          }
+          </tbody>
+        </table>
+        </div>
+  
+       
+    );
+
+}
+
 export default GestionUsuarios;
